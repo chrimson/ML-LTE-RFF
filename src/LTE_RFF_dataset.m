0f2e1e6716a3df665768d3bf290227f18c08db1e
Chris Limson <chrimson@users.noreply.github.com>
2024-10-28 10:08:20 -0400

    Reinstate truncation, save to files

--- src/LTE_RFF_dataset.m
+++ src/LTE_RFF_dataset.m
@@ -1,7 +1,7 @@
-strength = 0.1; % Amplitude effect
+strength = 0.1; % Amplitude effect (0.1)
 trunc = 5000; % Adjusted length of waveform for development
-dev = 0.06; % Deviation from fingerprint parameters
-rep = 20; % Adjustable repetitive factor
+dev = 0.05; % Deviation from fingerprint parameters (0.05 is +/-0.025)
+rep = 50; % Adjustable repetitive factor (10-80 looks good)
 
 num_wf = 4;
 num_dev = 4;
@@ -26,17 +26,18 @@ in = [1; 0; 0; 1];
 
 % Generation
 [waveform, grid, cfg] = lteRMCULTool(cfg, in);
-wf_size = length(waveform);
-t = 1 : wf_size;
+waveform = waveform(1:trunc);
+wf_len = length(waveform);
+t = 1 : wf_len;
 
 
 %% RF Fingerprint
 base = 1 - dev / 2;
 for wf_iter = 1:num_wf
-    A = rep*pi*rand/wf_size;
-    B = rep*pi*rand/wf_size;
-    C = rep*pi*rand/wf_size;
-    D = rep*pi*rand/wf_size;
+    A = rep*pi*rand/wf_len;
+    B = rep*pi*rand/wf_len;
+    C = rep*pi*rand/wf_len;
+    D = rep*pi*rand/wf_len;
     M = rand() * strength;
     N = rand() * strength;
     
@@ -56,9 +57,9 @@ for wf_iter = 1:num_wf
             (base+rand*dev)*N*cos((base+rand*dev)*C*t + (base+rand*dev)*D);
         plot(t, rff);
         hold on;
-    
-        waveform = waveform .* rff';
-        
+
+        wf = waveform .* rff';
+
         max=-1;
         list = dir(mac);
         for i = 1:length(list)
@@ -70,26 +71,27 @@ for wf_iter = 1:num_wf
                 end
             end
         end
-        
         fid = fopen(sprintf('%s/%04d', mac, max+1), 'w');
-        fprintf(fid, '%.4f + %.4fi\n', [real(waveform(:)), imag(waveform(:))].');
+        fprintf(fid, '%.4f + %.4fi\n', [real(wf(:)), imag(wf(:))].');
         fclose(fid);
+
     end
 end
 
 %{
+%% Visualize
 % Specify the sample rate of the waveform in Hz
 Fs = cfg.SamplingRate;
 
-%% Visualize
 % Time Scope
 timeScope = timescope('SampleRate', Fs, ...
     'TimeSpanOverrunAction', 'scroll', ...
     'TimeSpanSource', 'property', ...
     'TimeSpan', 9.7656e-07);
 
-timeScope(waveform);
-%timeScope(rff);
+%timeScope(waveform);
+%timeScope(rff');
+timeScope(wf);
 release(timeScope);
 
 % Spectrum Analyzer
