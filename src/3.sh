ed46ee5820109f148dbeba6d88efc160e202d159
Chris Limson <chrimson@gmail.com>
2024-11-06 13:03:05 +0000

    Update documentation

--- src/3.sh
+++ src/3.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-matlab -nodisplay -nodesktop -nosplash -r "s3_LTE_RWF_test($1, $2); exit;"
+matlab -nodisplay -nodesktop -nosplash -r "s3_LTE_RWF_test($1, $2, $3, $4, $5, $6, $7, $8); exit;"
