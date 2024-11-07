415bdad8956d27f213e18525ee33ee7c63961eb0
Chris Limson <chrimson@gmail.com>
2024-11-07 20:35:19 +0000

    Reformat

--- src/all.sh
+++ src/all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+MAC=33-04-E7-92-52-BD
 
 for rep in 10 15 20 25 30 35
 do
@@ -10,9 +11,14 @@ do
 
          python3 s2_BuildTrainCNN.py $rep $str
 
-         PARMS=$(grep 33-04-E7-92-52-BD ${rep}x${str}_ue_rwf_parm.asc | awk '{ print $2 $3 $4 $5 $6 $7 }')
+         A=$(grep ${MAC} ${rep}x${str}_ue_rwf_parm.asc | awk '{ print $2 }')
+         B=$(grep ${MAC} ${rep}x${str}_ue_rwf_parm.asc | awk '{ print $3 }')
+         C=$(grep ${MAC} ${rep}x${str}_ue_rwf_parm.asc | awk '{ print $4 }')
+         D=$(grep ${MAC} ${rep}x${str}_ue_rwf_parm.asc | awk '{ print $5 }')
+         J=$(grep ${MAC} ${rep}x${str}_ue_rwf_parm.asc | awk '{ print $6 }')
+         K=$(grep ${MAC} ${rep}x${str}_ue_rwf_parm.asc | awk '{ print $7 }')
 
-         matlab -nodisplay -nodesktop -nosplash -r "s3_LTE_RWF_test($rep, $str, $PARMS); exit;"
+         matlab -nodisplay -nodesktop -nosplash -r "s3_LTE_RWF_test($rep, $str, $A, $B, $C, $D, $J, $K); exit;"
 
          python3 s4_MatchUEbyRWF.py $rep $str > ${rep}x${str}_ue_rwf_match.log
 
