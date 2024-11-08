196c9ff202deb9158c338e9b4ea506a2addc7947
Chris Limson <chrimson@gmail.com>
2024-11-08 19:21:26 +0000

    Separate update only from conditionally

--- src/5.sh
+++ src/5.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 s5_UpdateCNN.py $1 $2 BB-69-F8-5C-96-7E
+python3 s5_UpdateCNN.py $1 $2 $3
--- src/s5_UpdateCNN.py
+++ src/s5_UpdateCNN.py
@@ -22,31 +22,21 @@ cnn_file = f'{rep}x{stg}_rwf_cnn.keras'
 rwf_file = f'{rep}x{stg}_target_rwf.asc'
 le = joblib.load(f'{rep}x{stg}_mac_label_enc.pkl')
 
-print('Load RWF CNN', flush=True)
 model = load_model(cnn_file)
 
-print('Build one target RWF from file', flush=True)
 rwf = []
 with open(rwf_file) as file:
   for line in file:
     cpx = re.sub('[+ij]', '', line).split()
     rwf.append([float(cpx[0]), float(cpx[1])])
 
-print('Convert lists of one target to NumPy arrays', flush=True)
-# Move zero-centric to [0,1] normalization
 with open(norm_file) as file:
   norm = float(file.read())
 RWF = np.array([rwf]) * norm + 0.5
 
-print('Label encoding', flush=True)
-#le = LabelEncoder()
-#le.classes_ = np.append(le.classes_, mac)
-#joblib.dump(le, f'{rep}x{stg}_mac_label_enc.pkl')
-#MAC = np.array([le.fit_transform(le.classes_)[-1]])
 MAC = le.transform([mac])
 
-print('Retraining, save', flush=True)
-#model.fit(RWF, MAC, validation_split=0.2, batch_size=16, epochs=10)
+print('Retraining', flush=True)
 model.fit(RWF, MAC, batch_size=16, epochs=1)
 model.save(cnn_file)
 
